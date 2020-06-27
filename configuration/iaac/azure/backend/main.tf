991eed95758b56a890c74263877a9cd78283f040
techvimal <techvimal19@gmail.com>
2020-06-27 19:02:05 +0800

    Fixing Terraform CLI error

--- configuration/iaac/azure/backend/main.tf
+++ configuration/iaac/azure/backend/main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
-  version         = ">= 2.0"
-  features {}
   client_id       = var.client_id   # ENVIRONMENT VARIABLE
   client_secret   = var.client_secret # ENVIRONMENT VARIABLE
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
+  version = "=2.0.0"
+  features {}
 }
 
 resource "azurerm_resource_group" "resource_group" {
